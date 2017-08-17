fa0bba87fe6ab69bd9d902d24ff7df5dabd620a2
Ezekiel Bethel <mctinfoilball@gmail.com>
2017-08-17 14:30:39 +0100

    dump before fatInit, warnings.

--- source/dump.c
+++ source/dump.c
@@ -7,8 +7,9 @@
 #include <math.h>
 #include <stdint.h>
 
-void relocate(uint32_t *dat, uint32_t len, uint32_t off, uint32_t a, uint32_t b)
+void relocate(uint32_t addr, uint32_t len, uint32_t off, uint32_t a, uint32_t b)
 {
+	uint32_t *dat = (uint32_t*)addr;
 	for(int j = 0; j < len/4; j++,dat++)
 	{
 		uint32_t v = *dat;
@@ -24,94 +25,96 @@ int main(int argc, char **argv) {
 	uint32_t *dldi_storage =   (uint32_t*)dldi_storage_addr;
 	// Initialise the console, required for printf
 	consoleDemoInit();
-	
-	if (fatInitDefault()) {
-		iprintf("Finding DLDI signature.. (%08x?)", *(uint32_t*)0x02000b40);
-		uint32_t *scan_start = (unsigned int*)0x02000000;
-		uint32_t *scan_end =   (unsigned int*)0x02001000;
 
-		for(uint32_t *scan = scan_start; scan < scan_end; scan++)
+	iprintf("Finding DLDI signature..");
+	uint32_t *scan_start = (uint32_t*)0x02000000;
+	uint32_t *scan_end =   (uint32_t*)0x02001000;
+
+	int size = 0;
+
+	for(uint32_t *scan = scan_start; scan < scan_end; scan++)
+	{
+		if(*scan == 0xBF8DA5ED)
 		{
-			if(*scan == 0xBF8DA5ED)
+			iprintf("at %08lx\n", (uint32_t)scan);
+			char *dldi_base = (char*)scan;
+
+			iprintf("Card: %s\n", dldi_base + 16);
+
+			int log2_size = *(dldi_base + 13);
+			size = 1<<log2_size;
+			iprintf("log2 size: %i (%i bytes)\n", log2_size, size);
+
+			memcpy(dldi_storage, scan, size);
+
+			uint32_t curr_addr = (uint32_t)dldi_base;
+			uint32_t curr_end = curr_addr + size;
+			uint32_t new_base = 0xBF800000;
+			uint32_t reloc_off = new_base - curr_addr;
+
+			iprintf("reloc_off: %08lx.\n", reloc_off);
+
+			uint32_t section_starts[4];
+			uint32_t section_ends[4];
+			uint32_t section_sizes[4];
+
+			uint32_t *section_table = dldi_storage + 0x10;
+
+			for(int i = 0; i < 4; i++,section_table+=2)
 			{
-				iprintf("at %08x\n", scan);
-				char *dldi_base = (char*)scan;
-
-				iprintf("Card: %s\n", dldi_base + 16);
-				
-				int log2_size = *(dldi_base + 13);
-				int size = 1<<log2_size;
-				iprintf("log2 size: %i (%i bytes)\n", log2_size, size);
-				
-				memcpy(dldi_storage, scan, size);
-
-				uint32_t curr_addr = (uint32_t)dldi_base;
-				uint32_t curr_end = curr_addr + size;
-				uint32_t new_base = 0xBF800000;
-				uint32_t reloc_off = new_base - curr_addr;
-
-				iprintf("reloc_off: %08x.\n", reloc_off);
-
-				uint32_t section_starts[4];
-				uint32_t section_ends[4];
-				uint32_t section_sizes[4];
-
-				uint32_t *section_table = dldi_storage + 0x10;
-
-				for(int i = 0; i < 4; i++,section_table+=2)
-				{
-					section_starts[i] = (*section_table) - curr_addr + dldi_storage_addr;
-					section_ends[i] = *(section_table + 1) - curr_addr + dldi_storage_addr;
-					section_sizes[i] = section_ends[i] - section_starts[i];
-					iprintf("Section %i %08x=>%08x (%08x).\n", i, section_starts[i], section_ends[i], section_sizes[i]);
-				}
-
-				// Fix sections
-				for(int i = 0; i < 8; i++)
-				{
-					*(dldi_storage + 0x10 + i) += reloc_off;
-				}
-
-				// Fix function pointers
-				for(int i = 0; i < 6; i++)
-				{
-					*(dldi_storage + 0x1a + i) += reloc_off;
-				}
-
-				int sections = *(dldi_base + 14);
-				iprintf("Sections %08x.\n", sections);
-
-				if(sections & 1) // Data references.
-				{
-					iprintf("Relocating data %08x=>%08x.\n", section_starts[0], section_ends[0]);
-					relocate(section_starts[0], section_sizes[0], reloc_off, curr_addr, curr_end);
-				}
-
-				if(sections & 2) // Glue references.
-				{
-					iprintf("Relocating glue %08x=>%08x\n", section_starts[1], section_ends[1]);
-					relocate(section_starts[1], section_sizes[1], reloc_off, curr_addr, curr_end);
-				}
-
-				if(sections & 4) // GOT references.
-				{
-					iprintf("Relocating got %08x=>%08x\n", section_starts[2], section_ends[2]);
-					relocate(section_starts[2], section_sizes[2], reloc_off, curr_addr, curr_end);
-				}
-
-				if(sections & 8) // clear BSS
-				{
-					memset(section_starts[3], 0, section_sizes[3]);
-				}
-
-				iprintf("Writing...\n");
-				FILE *f = fopen("dldi.bin", "wb");
-				fwrite(dldi_storage, 1, size, f);
-				fclose(f);
-				break;
+				section_starts[i] = (*section_table) - curr_addr + dldi_storage_addr;
+				section_ends[i] = *(section_table + 1) - curr_addr + dldi_storage_addr;
+				section_sizes[i] = section_ends[i] - section_starts[i];
+				iprintf("Section %i %08lx=>%08lx (%08lx).\n", i, section_starts[i], section_ends[i], section_sizes[i]);
 			}
-		}
 
+			// Fix sections
+			for(int i = 0; i < 8; i++)
+			{
+				*(dldi_storage + 0x10 + i) += reloc_off;
+			}
+
+			// Fix function pointers
+			for(int i = 0; i < 6; i++)
+			{
+				*(dldi_storage + 0x1a + i) += reloc_off;
+			}
+
+			int sections = *(dldi_base + 14);
+			iprintf("Sections %08x.\n", sections);
+
+			if(sections & 1) // Data references.
+			{
+				iprintf("Relocating data %08lx=>%08lx.\n", section_starts[0], section_ends[0]);
+				relocate(section_starts[0], section_sizes[0], reloc_off, curr_addr, curr_end);
+			}
+
+			if(sections & 2) // Glue references.
+			{
+				iprintf("Relocating glue %08lx=>%08lx\n", section_starts[1], section_ends[1]);
+				relocate(section_starts[1], section_sizes[1], reloc_off, curr_addr, curr_end);
+			}
+
+			if(sections & 4) // GOT references.
+			{
+				iprintf("Relocating got %08lx=>%08lx\n", section_starts[2], section_ends[2]);
+				relocate(section_starts[2], section_sizes[2], reloc_off, curr_addr, curr_end);
+			}
+
+			if(sections & 8) // clear BSS
+			{
+				memset((uint32_t*)section_starts[3], 0, section_sizes[3]);
+			}
+
+			break;
+		}
+	}
+
+	if (fatInitDefault()) {
+		iprintf("Writing...\n");
+		FILE *f = fopen("dldi.bin", "wb");
+		fwrite(dldi_storage, 1, size, f);
+		fclose(f);
 	} else {
 		iprintf("fatInitDefault failure: terminating\n");
 	}
